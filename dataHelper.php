b3a202a7a347c49efab600afd160411dbe143e52
Jorge <jorge6242@gmail.com>
2020-07-17 07:08:42 -0400

    validation to denied errors into production environment

--- dataHelper.php
+++ dataHelper.php
@@ -1,6 +1,21 @@
 <?php 
 	//echo $_SERVER['REQUEST_URI'];
 
+	// Turn off error reporting
+	error_reporting(0);
+
+	// Report runtime errors
+	error_reporting(E_ERROR | E_WARNING | E_PARSE);
+
+	// Report all errors
+	error_reporting(E_ALL);
+
+	// Same as error_reporting(E_ALL);
+	ini_set("error_reporting", E_ALL);
+
+	// Report all errors except E_NOTICE
+	error_reporting(E_ALL & ~E_NOTICE);
+
 	include 'wsLibrary.php';
 	
 	// require __DIR__ . '/application/vendor/autoload.php';
